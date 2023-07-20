7396a54ef4ac237fcd501d63385f63ddf20cc8fe
Jason Plumb <jplumb@splunk.com>
2023-07-20 15:32:00 -0700

    update for version 4 and use OpenTelemetry

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,15 @@ application {
         "-javaagent:splunk-otel-javaagent-1.26.0.jar",
         "-Dotel.javaagent.debug=true",
         "-Dotel.instrumentation.netty.enabled=false",
+        "-Dotel.instrumentation.netty-4.1.enabled=false",
         "-Dotel.service.name=VertxExample"
     )
 }
 
 dependencies {
     implementation("io.vertx:vertx-web:4.4.4")
+//    implementation("io.vertx:vertx-web:3.9.16")
     implementation("io.vertx:vertx-codegen:4.4.4")
+//    implementation("io.vertx:vertx-codegen:3.9.16")
+    implementation("io.vertx:vertx-opentelemetry:4.4.4")
 }
--- src/main/java/com/splunk/example/VertxApplication.java
+++ src/main/java/com/splunk/example/VertxApplication.java
@@ -1,12 +1,14 @@
 package com.splunk.example;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.tracing.opentelemetry.OpenTelemetryOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,9 +17,51 @@ public class VertxApplication {
     public static final int PORT = 9080;
 
     public static void main(String[] args) {
-        Vertx vertx = Vertx.vertx();
+        OpenTelemetry otel = GlobalOpenTelemetry.get();
+        VertxOptions options = new VertxOptions()
+                .setTracingOptions(new OpenTelemetryOptions(otel));
+        Vertx vertx = Vertx.vertx(options);
         HttpServer server = vertx.createHttpServer();
 
+        Router router = buildRouter(vertx);
+        server.requestHandler(router).listen(PORT);
+        System.out.println("Server is running on port " + PORT);
+    }
+
+    static Router buildRouter(Vertx vertx) {
+//        return buildVersion3Router(vertx);
+        return buildVersion4Router(vertx);
+    }
+//
+//    static Router buildVersion3Router(Vertx vertx) {
+//        Router router = Router.router(vertx);
+//        router
+//                .get("/hello").handler(ctx -> {
+//                    try {
+//                        TimeUnit.MILLISECONDS.sleep(500);
+//                        String body = new JsonObject().put("hello", "from vertx-web").encode();
+//                        ctx.response().setStatusCode(200)
+//                                .putHeader("Content-Type", "application/json")
+//                                .end(body);
+//                    } catch (InterruptedException e) {
+//                        ctx.response().setStatusCode(500)
+//                                .putHeader("Content-Type", "text/plain")
+//                                .end("interrupted a bunch");
+//                    }
+//                });
+//        router.get("/error").handler(new Handler<RoutingContext>() {
+//            @Override
+//            public void handle(RoutingContext event) {
+//                if (1 == 1) {
+//                    throw new RuntimeException("boom");
+//                }
+//            }
+//        });
+//        return router;
+//
+//    }
+
+    private static Router buildVersion4Router(Vertx vertx) {
         Router router = Router.router(vertx);
         router
                 .get("/hello")
@@ -30,14 +74,12 @@ public class VertxApplication {
                     }
                 });
         router.get("/error")
-                        .respond( ctx -> {
-                            if(1 == 1){
-                                throw new RuntimeException("boom");
-                            }
-                            return Future.failedFuture("ouch");
-                        });
-
-        server.requestHandler(router).listen(PORT);
-        System.out.println("Server is running on port " + PORT);
+                .respond(ctx -> {
+                    if (1 == 1) {
+                        throw new RuntimeException("boom");
+                    }
+                    return Future.failedFuture("ouch");
+                });
+        return router;
     }
 }
